5fa34ad6bb99a9ea072a4e9709590fc489db97ff
aofattaporn <aofattapon321@gmail.com>
2024-02-18 15:45:04 +0700

    Initial Commit

---
+++ MyPlayList/MyPlayListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlayListApp.swift
+//  MyPlayList
+//
+//  Created by attaporn on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPlayListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
